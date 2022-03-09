d5b1f7d5a4f18a28b2b5da7030463cc6523f9713
weiyangw <weiyangw@mit.edu>
2022-03-09 19:31:29 +0000

    general script

--- run_a100.sh
+++ run_a100.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
+chr() {
+  [ "$1" -lt 256 ] || return 1
+  printf "\\$(printf '%03o' "$1")"
+}
+
+ord() {
+  LC_CTYPE=C printf '%d' "'$1"
+}
+
 export WORLD_SIZE=12
-export RANK=0
+export RANK=$(( $(ord $(echo ${HOSTNAME:0:1})) - $(ord a) ))
+
 export MASTER_PORT=27182
 export MASTER_ADDR="abtin.csail.mit.edu"
 
@@ -18,5 +28,15 @@ export NCCL_MIN_NCHANNELS=2
 export NCCL_MAX_NCHANNELS=2
 export NCCL_ALGO=Ring
 
-python3 dlrm_s_pytorch.py  --arch-embedding-size 10000-10000-10000-10000-10000-10000-10000-10000-10000-10000-10000-10000 --mini-batch-size 12 --nepochs 10 --num-batches 16 --use-gpu
+eval "$($HOME/miniconda/bin/conda shell.bash hook)"
+conda init
+conda activate torch
+
+if [[ $RANK != 0 ]]; then
+  sleep 5
+else
+  printenv
+fi
+
+python3 dlrm_s_pytorch.py --arch-mlp-bot 2048-2048-2048-2048-2048-2048-2048-2048 --arch-mlp-top 1024-1024-1024-1024-1 --arch-interaction-op cat  --arch-embedding-size 10000000-10000000-10000000-10000000-10000000-10000000-10000000-10000000-10000000-10000000-10000000-10000000 --mini-batch-size 6144 --nepochs 5 --num-batches 16384 --use-gpu
 
