b0d78d3abad6e7ef69f616af9b13d0ab39279242
weiyangw <weiyangw@mit.edu>
2022-03-14 22:39:52 +0000

    update script

--- run_a100stupid.sh
+++ run_a100stupid.sh
@@ -21,20 +21,21 @@ export MASTER_ADDR="abtin.csail.mit.edu"
 
 export NCCL_NET="IB"
 export NCCL_IB_GID_INDEX=3
-export NCCL_IB_HCA="qedr0,qedr1,qedr2,qedr3"
-export LD_LIBRARY_PATH="/home/frankwwy/mccl/build/lib:"
-export LD_PRELOAD="/home/frankwwy/mccl/build/lib/libnccl.so:"
+export NCCL_IB_HCA="mlx5_0"
+#export NCCL_IB_HCA="qedr0,qedr1,qedr2,qedr3"
+export LD_LIBRARY_PATH="/home/frankwwy/nccl/build/lib:"
+export LD_PRELOAD="/home/frankwwy/nccl/build/lib/libnccl.so:"
 export NCCL_MIN_NCHANNELS=4
 export NCCL_MAX_NCHANNELS=4
 export NCCL_ALGO=Ring
 
-runname=stupid
+runname=stupidft
 embedding_dim=32768
 mlp_bot=2048-2048-2048-2048-2048-2048-2048-$embedding_dim
 mlp_top=1024-1024-1024-1024-1
 embedding_sz=100000
 prof=""
-declare -a l_batch_szs=(1024)
+declare -a l_batch_szs=(16 32 64 128 256 512 1024)
 
 eval "$($HOME/miniconda/bin/conda shell.bash hook)"
 conda init
