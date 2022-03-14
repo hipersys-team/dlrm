9e0f5045513064187c179d48e3d83293f4e58bf8
weiyangw <weiyangw@mit.edu>
2022-03-14 19:19:46 +0000

    update script

--- run_a100.sh
+++ run_a100.sh
@@ -22,13 +22,13 @@ export MASTER_ADDR="abtin.csail.mit.edu"
 export NCCL_NET="IB"
 export NCCL_IB_GID_INDEX=3
 export NCCL_IB_HCA="qedr0,qedr1,qedr2,qedr3"
-export LD_LIBRARY_PATH="/home/frankwwy/mccl/build/lib:"
-export LD_PRELOAD="/home/frankwwy/mccl/build/lib/libnccl.so:"
+export LD_LIBRARY_PATH="/home/frankwwy/nccl/build/lib:"
+export LD_PRELOAD="/home/frankwwy/nccl/build/lib/libnccl.so:"
 export NCCL_MIN_NCHANNELS=4
 export NCCL_MAX_NCHANNELS=4
 export NCCL_ALGO=Ring
 
-runname=small
+runname=smallft
 embedding_dim=4096
 mlp_bot=4096-4096-4096-$embedding_dim
 mlp_top=1024-1024-1024-1024-1
@@ -43,12 +43,14 @@ conda activate torch
 for l_batch_sz in "${l_batch_szs[@]}"; do
   if [[ $RANK != 0 ]]; then
     sleep 5
-    python3 dlrm_s_pytorch.py --dist-backend="nccl" --arch-mlp-bot $mlp_bot --arch-sparse-feature-size $embedding_dim --arch-mlp-top $mlp_top --arch-interaction-op dot --arch-embedding-size ${embedding_sz}-${embedding_sz}-${embedding_sz}-${embedding_sz}-${embedding_sz}-${embedding_sz}-${embedding_sz}-${embedding_sz}-${embedding_sz}-${embedding_sz}-${embedding_sz}-${embedding_sz} --mini-batch-size $(( 12 * $l_batch_sz )) --nepochs 5 --num-batches 64 --use-gpu --print-time --dataset-multiprocessing $prof
+    python3 dlrm_s_pytorch.py --dist-backend="nccl" --arch-mlp-bot $mlp_bot --arch-sparse-feature-size $embedding_dim --arch-mlp-top $mlp_top --arch-interaction-op dot --arch-embedding-size ${embedding_sz}-${embedding_sz}-${embedding_sz}-${embedding_sz}-${embedding_sz}-${embedding_sz}-${embedding_sz}-${embedding_sz}-${embedding_sz}-${embedding_sz}-${embedding_sz}-${embedding_sz} --mini-batch-size $(( 12 * $l_batch_sz )) --nepochs 5 --num-batches 64 --use-gpu --print-time --dataset-multiprocessing $prof > /dev/null 2>&1
   else
     printenv
     python3 dlrm_s_pytorch.py --dist-backend="nccl" --arch-mlp-bot $mlp_bot --arch-sparse-feature-size $embedding_dim --arch-mlp-top $mlp_top --arch-interaction-op dot --arch-embedding-size ${embedding_sz}-${embedding_sz}-${embedding_sz}-${embedding_sz}-${embedding_sz}-${embedding_sz}-${embedding_sz}-${embedding_sz}-${embedding_sz}-${embedding_sz}-${embedding_sz}-${embedding_sz} --mini-batch-size $(( 12 * $l_batch_sz )) --nepochs 5 --num-batches 64 --use-gpu --print-time --dataset-multiprocessing $prof > result_${runname}_${l_batch_sz}.res
   fi
-  sleep 450
+  sleep 300
+  killall ssh
+  sleep 100
 done
 
 
