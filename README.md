ec8436d35d42f50e6fb09a6bc82ce66018089f9f
Weiyang Wang <weiyangw@mit.edu>
2022-07-11 21:50:42 -0400

    update readme and scripts

--- README.md
+++ README.md
@@ -3,3 +3,15 @@
 This submodule contains the DLRM model used to generate TopoOpt's testbed DLRM result. It was adopted from Meta's implementaiton of DLRM with model parallelism. Please check the original README file at [here](DLRM_README.md) for a more detailed description. 
 
 ## Instructions
+The scripts are designed to run distributed model parallel training of DLRM on MIT's 12 node cluster testbed. Please check [this document](https://docs.google.com/document/d/190nelkTXo7fEQNWRe4rnMglzAvV1jj-ZyShMcAGZH08/edit?usp=sharing) for how to setup RDMA forwarding and using the hacked version of NCCL. 
+
+The scripts uses the [pytorch version](dlrm_s_pytorch.py) of this repository. 
+
+`run_a100_fattree.sh` will run DLRM training on the Mellanox ConnectX5 NIC, which are connected to a single switch. 
+
+`run_a100_topoopt.sh` will run DLRM training on the HPE nics, which are connected to the patch panel. Please be sure the forwarding is setup properly before running this test. Check the document above on how to setup RDMA fowarding. 
+
+To execute the program, adjust the parameters in the scripts and run the script on ALL of the workers. The script will automatically pick the master and log the training output on the master machine. 
+
+
+
--- run_a100_topoopt.sh
+++ run_a100_topoopt.sh
@@ -19,27 +19,23 @@ export MASTER_ADDR="abtin.csail.mit.edu"
 #export NCCL_SOCKET_IFNAME="enp3s0f0"
 #export NCCL_DEBUG="INFO"
 
-#export NCCL_NET="IB"
-#export NCCL_IB_GID_INDEX=3
-#export NCCL_IB_HCA="qedr0,qedr1,qedr2,qedr3"
-#export NCCL_IB_HCA="mlx5_0"
-export NCCL_IB_DISABLE=1
-export NCCL_SOCKET_IFNAME="enp3s0f0"
-export LD_LIBRARY_PATH="/home/frankwwy/nccl/build/lib:"
-export LD_PRELOAD="/home/frankwwy/nccl/build/lib/libnccl.so:"
+export NCCL_NET="IB"
+export NCCL_IB_GID_INDEX=3
+# export NCCL_IB_HCA="mlx5_0"
+export NCCL_IB_HCA="qedr0,qedr1,qedr2,qedr3"
+export LD_LIBRARY_PATH="/home/frankwwy/mccl/build/lib:"
+export LD_PRELOAD="/home/frankwwy/mccl/build/lib/libnccl.so:"
 export NCCL_MIN_NCHANNELS=4
 export NCCL_MAX_NCHANNELS=4
 export NCCL_ALGO=Ring
 
-
-runname=stupidft
+runname=paper
 embedding_dim=32768
 mlp_bot=2048-2048-2048-2048-2048-2048-2048-$embedding_dim
 mlp_top=1024-1024-1024-1024-1
 embedding_sz=100000
 prof=""
-
-declare -a l_batch_szs=(16)
+declare -a l_batch_szs=(16 32 64 128 256 512 1024)
 
 eval "$($HOME/miniconda/bin/conda shell.bash hook)"
 conda init
@@ -51,11 +47,9 @@ for l_batch_sz in "${l_batch_szs[@]}"; do
     python3 dlrm_s_pytorch.py --dist-backend="nccl" --arch-mlp-bot $mlp_bot --arch-sparse-feature-size $embedding_dim --arch-mlp-top $mlp_top --arch-interaction-op dot --arch-embedding-size ${embedding_sz}-${embedding_sz}-${embedding_sz}-${embedding_sz}-${embedding_sz}-${embedding_sz}-${embedding_sz}-${embedding_sz}-${embedding_sz}-${embedding_sz}-${embedding_sz}-${embedding_sz} --mini-batch-size $(( 12 * $l_batch_sz )) --nepochs 5 --num-batches 64 --use-gpu --print-time --dataset-multiprocessing $prof > /dev/null 2>&1
   else
     printenv
-    python3 dlrm_s_pytorch.py --dist-backend="nccl" --arch-mlp-bot $mlp_bot --arch-sparse-feature-size $embedding_dim --arch-mlp-top $mlp_top --arch-interaction-op dot --arch-embedding-size ${embedding_sz}-${embedding_sz}-${embedding_sz}-${embedding_sz}-${embedding_sz}-${embedding_sz}-${embedding_sz}-${embedding_sz}-${embedding_sz}-${embedding_sz}-${embedding_sz}-${embedding_sz} --mini-batch-size $(( 12 * $l_batch_sz )) --nepochs 5 --num-batches 64 --use-gpu --print-time --dataset-multiprocessing $prof #> result_${runname}_${l_batch_sz}.res
+    python3 dlrm_s_pytorch.py --dist-backend="nccl" --arch-mlp-bot $mlp_bot --arch-sparse-feature-size $embedding_dim --arch-mlp-top $mlp_top --arch-interaction-op dot --arch-embedding-size ${embedding_sz}-${embedding_sz}-${embedding_sz}-${embedding_sz}-${embedding_sz}-${embedding_sz}-${embedding_sz}-${embedding_sz}-${embedding_sz}-${embedding_sz}-${embedding_sz}-${embedding_sz} --mini-batch-size $(( 12 * $l_batch_sz )) --nepochs 5 --num-batches 64 --use-gpu --print-time --dataset-multiprocessing $prof > result_${runname}_${l_batch_sz}.res
   fi
-  sleep 300
-  killall ssh
-  sleep 100
+  sleep 450
 done
 
 
